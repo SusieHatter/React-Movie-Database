3cc0733dc4026a16323941f074e4b311d6eb61e8
SusieHatter <susiivaldez@gmail.com>
2021-11-10 04:41:34 -0600

    Add GlobalStyles component with general styling set, also add Header component with respective styling and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+//Components
+import Header from "./components/Header";
+
+//styles
+import { GlobalStyle } from "./GlobalStyles";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <Header />
+      Hello world!
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
