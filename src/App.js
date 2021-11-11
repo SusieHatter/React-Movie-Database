0458b6c68f850f720e1895d095aded72f225c934
SusieHatter <susiivaldez@gmail.com>
2021-11-11 06:11:05 -0600

    Add Home component that will display the movies from the API database

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 //Components
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 //styles
 import { GlobalStyle } from "./GlobalStyles";
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      Hello world!
+      <Home />
       <GlobalStyle />
     </div>
   );
